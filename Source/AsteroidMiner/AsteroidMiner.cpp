0f07b14d0093e6672f1378692445518a799761bc
Altaezio <v.robert.jaunet@gmail.com>
2023-02-18 19:12:45 +0100

    unreal initial commit

---
+++ Source/AsteroidMiner/AsteroidMiner.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AsteroidMiner.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AsteroidMiner, "AsteroidMiner" );
