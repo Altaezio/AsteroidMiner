f45bb30ebd3e45f9f0e893219e2da84fc39c8731
Altaezio <v.robert.jaunet@gmail.com>
2023-02-18 20:01:33 +0100

    Actions

---
+++ Source/AsteroidMiner/Private/ShipController.cpp
@@ -0,0 +1,81 @@
+#include "ShipController.h"
+#include "EnhancedInput/Public/InputMappingContext.h"
+#include "EnhancedInput/Public/EnhancedInputSubsystems.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+// Sets default values
+AShipController::AShipController()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	// Instantiating your class Components
+
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+
+	//Set the location and rotation of the Character Mesh Transform
+
+	GetMesh()->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -90.0f), FQuat(FRotator(0.0f, -90.0f, 0.0f)));
+
+	// Attaching your class Components to the default character's Skeletal Mesh Component.
+
+	SpringArmComp->SetupAttachment(GetMesh());
+
+	CameraComp->SetupAttachment(SpringArmComp, USpringArmComponent::SocketName);
+
+	//Setting class variables of the spring arm
+
+	SpringArmComp->bUsePawnControlRotation = true;
+
+	//Setting class variables of the Character movement component
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+
+	GetCharacterMovement()->bUseControllerDesiredRotation = true;
+
+	GetCharacterMovement()->bIgnoreBaseRotation = true;
+}
+
+// Called when the game starts or when spawned
+void AShipController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AShipController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AShipController::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	// Get the player controller
+	APlayerController* PC = Cast<APlayerController>(GetController());
+
+	// Get the local player subsystem
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer());
+	// Clear out existing mapping, and add our mapping
+	Subsystem->ClearAllMappings();
+	Subsystem->AddMappingContext(InputMapping, 0);
+}
+
+void AShipController::MoveForward(float AxisValue)
+{
+	if ((Controller != nullptr) && (AxisValue != 0.0f))
+	{
+		// Find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// Get forward vector
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, AxisValue);
+	}
+}
